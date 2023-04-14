dd7cc5ec568f51c1c7e0f738741f429948606a2d
StokeyBear <122556101+StokeyBear@users.noreply.github.com>
2023-04-13 20:02:35 -0500

    Add files via upload

---
+++ Script/script.js
@@ -0,0 +1,16 @@
+$(function () 
+  { 
+
+//placeholder because I don't really know what goes here right now
+
+}
+ 
+ function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+// I don't believe the function works because of the documentation labels not matching 
